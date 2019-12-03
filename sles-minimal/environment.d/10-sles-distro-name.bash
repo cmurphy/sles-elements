30ad3c92fb279de97b914bc497fd37f449c98bd8
Colleen Murphy <colleen.murphy@suse.de>
2019-12-02 17:20:55 -0800

    Clean up sles-minimal element
    Remove the zypper script copied from the zypper-minimal element, the
    original will work with a few variable overrides[1]. Clean up
    dependencies.
    
    [1] https://review.opendev.org/696990

--- sles-minimal/environment.d/10-sles-distro-name.bash
+++ sles-minimal/environment.d/10-sles-distro-name.bash
@@ -1,5 +1,12 @@
 # pretend to be opensuse so the python element works
 export DISTRO_NAME=opensuse
-#DIB_RELEASE=${DIB_RELEASE:-42.3}
-#export DIB_RELEASE=${DIB_RELEASE,,}
-export DIB_SLES_PATTERNS=patterns-sles-base
+DIB_RELEASE=${DIB_RELEASE:-15-1}
+export DIB_RELEASE=${DIB_RELEASE,,}
+export DIB_OPENSUSE_PATTERNS="patterns-sles-base sles-release"
+export DIB_INIT_SYSTEM=systemd
+
+DIB_ZYPPER_REPOS="update=>http://download.suse.de/ibs/SUSE:/SLE-15-SP1:/Update/standard/ "
+DIB_ZYPPER_REPOS+="SLE-15-SP1=>http://download.suse.de/ibs/SUSE:/SLE-15-SP1:/GA/standard/ "
+DIB_ZYPPER_REPOS+="SLE-15=>http://download.suse.de/ibs/SUSE:/SLE-15:/GA/standard/ "
+
+export DIB_ZYPPER_REPOS=${DIB_ZYPPER_REPOS}
