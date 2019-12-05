c8b03dc59c873dcd93688ba191655a228cd7bfb5
Colleen Murphy <colleen.murphy@suse.de>
2019-12-04 21:17:37 -0800

    Add SLE-15:Update repo

--- sles-minimal/environment.d/10-sles-distro-name.bash
+++ sles-minimal/environment.d/10-sles-distro-name.bash
@@ -8,5 +8,6 @@ export DIB_INIT_SYSTEM=systemd
 DIB_ZYPPER_REPOS="update=>http://download.suse.de/ibs/SUSE:/SLE-15-SP1:/Update/standard/ "
 DIB_ZYPPER_REPOS+="SLE-15-SP1=>http://download.suse.de/ibs/SUSE:/SLE-15-SP1:/GA/standard/ "
 DIB_ZYPPER_REPOS+="SLE-15=>http://download.suse.de/ibs/SUSE:/SLE-15:/GA/standard/ "
+DIB_ZYPPER_REPOS+="SLE-15-Update=>http://download.suse.de/ibs/SUSE:/SLE-15:/Update/standard/ "
 
 export DIB_ZYPPER_REPOS=${DIB_ZYPPER_REPOS}
