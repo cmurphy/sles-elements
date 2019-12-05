8e40917a1e953fa684fb0c77289ee7d019b93a66
Colleen Murphy <colleen.murphy@suse.de>
2019-12-05 13:46:28 -0800

    Add caasp-base element

---
+++ caasp-base/README.rst
@@ -0,0 +1,5 @@
+==========
+caasp-base
+==========
+
+Base requirements to run a development version of CaaSP v4 (skuba) on SLE15-SP1.
---
+++ caasp-base/environment.d/20-caasp-devel-repo.bash
@@ -0,0 +1,3 @@
+DIB_ZYPPER_REPOS+="suse-ca=>http://download.suse.de/ibs/SUSE:/CA/SLE_15_SP1/ "
+DIB_ZYPPER_REPOS+="devel-caasp-4.0=>http://download.suse.de/ibs/Devel:/CaaSP:/4.0/SLE_15_SP1/ "
+export DIB_ZYPPER_REPOS=${DIB_ZYPPER_REPOS}
